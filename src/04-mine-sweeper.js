130449416ef0d8e7cb8cfc7c36d2271748e00a3e
Yuriy Lipchey <xaosmmm@gmail.com>
2021-04-08 13:34:41 +0300

    feat: add solution for minesweeper task

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,54 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+
+function minesweeper(matrix) {
+  /* ----- function generate array of coordinates around given point ----- */
+  function generateCoords(indexI, indexJ, lengthI, lenghtJ) {
+    const result = [];
+    const initial = [-1, 1, 0];
+
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        const resI = indexI + initial[i];
+        const resJ = indexJ + initial[j];
+        if (resI >= 0 && resJ >= 0 && resI < lengthI && resJ < lenghtJ) {
+          result.push([resJ, resI]);
+        }
+      }
+    }
+    result.pop();
+    return result;
+  }
+
+  if (matrix.length === 0) return 0;
+
+  const lengthI = matrix.length;
+  const lengthJ = matrix[0].length;
+
+  /* ------ generate result array ---------*/
+  const resArr = [];
+  for (let i = 0; i < lengthI; i++) {
+    const tempArr = [];
+    for (let j = 0; j < lengthJ; j++) {
+      tempArr.push(0);
+    }
+    resArr.push(tempArr);
+  }
+
+  /* ----- generate mine sweper field ----- */
+  for (let i = 0; i < lengthI; i++) {
+    for (let j = 0; j < lengthJ; j++) {
+      const cellsArray = generateCoords(i, j, lengthI, lengthJ);
+
+      for (let k = 0; k < cellsArray.length; k++) {
+        const resI = cellsArray[k][0];
+        const resJ = cellsArray[k][1];
+        if (matrix[i][j]) resArr[resI][resJ]++;
+      }
+    }
+  }
+  return resArr;
 }
 
 module.exports = minesweeper;
