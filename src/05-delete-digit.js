97b57ee6ccf5ce3dda69e6c770d23c0d026c8592
Yuriy Lipchey <xaosmmm@gmail.com>
2021-04-08 14:15:07 +0300

    feat: add solution for delete number task

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,24 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+
+function deleteDigit(input) {
+  const inputArr = input.toString()
+    .split('');
+
+  let current = inputArr[0];
+  let found = false;
+  for (let i = 1; i < inputArr.length; i++) {
+    if (inputArr[i] > current) {
+      found = true;
+      inputArr.splice(i - 1, 1);
+      break;
+    }
+    current = inputArr[i];
+  }
+  if (!found) inputArr.pop();
+
+  return (+inputArr.join(''));
 }
 
 module.exports = deleteDigit;
