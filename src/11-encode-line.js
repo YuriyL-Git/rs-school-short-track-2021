5d1fb23154bce0bb7324bee31b2dc01f09c53635
Yuriy Lipchey <xaosmmm@gmail.com>
2021-04-11 15:02:46 +0300

    feat: add solution for task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,23 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  if (str === '') return '';
+
+  const inputArr = [...str.replace(/\s/g, '')];
+  inputArr.push(' ');
+  let result = '';
+  let currentChar = inputArr[0];
+  let counter = -1;
+  for (let i = 0; i < inputArr.length; i++) {
+    counter++;
+    if (inputArr[i] !== currentChar || i === inputArr.length - 1) {
+      result += counter === 1 ? currentChar : counter + currentChar;
+      counter = 0;
+      currentChar = inputArr[i];
+    }
+  }
+  return result;
 }
 
 module.exports = encodeLine;
